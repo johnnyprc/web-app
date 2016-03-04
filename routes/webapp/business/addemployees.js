99cc84867647ca2a9d8e1aa14498c471ed16a26c
Sean Ippolito <ippolitosean@gmail.com>
2016-03-03 23:33:14 -0800

    fixed post method in addemployees

--- routes/webapp/business/addemployees.js
+++ routes/webapp/business/addemployees.js
@@ -76,10 +76,16 @@ exports.post = function(req,res){
 
         for(var i = 0; i < rows.length; i++){
             var username = rows[i][0];
-            var email = rows[i][1];
+            console.log(rows[i][0]);
+            //console.log(rows[i][1]);
+            //var email = rows[i][1];
             var nameArr = username.split(' ');
 			var fname = nameArr[0];
+            console.log(fname);
 			var lname = nameArr[1];
+            console.log(lname);
+            var email = nameArr[2];
+            console.log(email);
             var token = randomToken();
             employeeDB.insert({
                 business: ObjectId(businessID),
@@ -109,8 +115,7 @@ exports.post = function(req,res){
             //    }
             //  });
         }
-        console.log('FUCKKKKDFDFSFSDFGSDFGSDFGSDGSDFGDSFGSDFGDSFGSDFGSDFGFdsg');
-        res.redirect('../id/dashboard');
+        res.redirect('../' + req.user[0].business + '/dashboard');
 }
 
 
