6c1b2902ba9811394e044fd96d962e08bfe7a1b1
Kurt Lang <kurtalang@gmail.com>
2016-03-07 23:12:42 -0800

    cleaned up code

--- routes/webapp/business/dashboard.js
+++ routes/webapp/business/dashboard.js
@@ -1,54 +1,16 @@
-var auth = require('../../../lib/auth');
-
 exports.get = function (req, res) {
-    console.log('Get funciton Dashboard');
-	var employeeId = req.user[0]._id;
-	var employeename = req.user[0].fname;
     console.log('Get function getemployees');
     var database = req.db;
     var employeeDB = database.get('employees');
     var bid = req.user[0].business;
-    var businesses = database.get('businesses');
-
-    //Arrays
-    var allEmps;
-    var peple =   [
-        "Yehuda Katz",
-        "Alan Johnson",
-        "Charles Jolley"
-    ];
-
 
     employeeDB.find( { business: bid })
-        .on('success', function(oneEmployee) {
-
-            allEmps = (oneEmployee);
-            console.log(allEmps);
+        .on('success', function(employees) {
 
             res.render('business/dashboard', {
-                emps: allEmps,
-                title: 'Express',
-                eid: employeeId,
-                employeeName: employeename,
+                emps: employees,
                 message: req.flash("permission")
             });
 
-
         });
-
-    console.log("break!");
-
-        //.forEach(function(oneEmployee) {
-        //    console.log( "user: " + oneEmployee.fname );
-        //})
-
-    //var fudge = allEmps[0].fname;
-    //console.log(fudge);
-    //res.render('business/dashboard', {
-    //    //emps: fudge,
-    //    title: 'Express',
-		//eid: employeeId,
-		//employeeName: employeename,
-		//message: req.flash("permission")
-    //});
 };
