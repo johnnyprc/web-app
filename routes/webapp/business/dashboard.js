dd4e13a3d5095e4ff112b929f2cb1c665c6c1c03
Kurt Lang <kurtalang@gmail.com>
2016-03-07 20:56:27 -0800

    Console all employees with BusID

--- routes/webapp/business/dashboard.js
+++ routes/webapp/business/dashboard.js
@@ -4,7 +4,15 @@ exports.get = function (req, res) {
     console.log('Get funciton Dashboard');
 	var employeeId = req.user[0]._id;
 	var employeename = req.user[0].fname;
-
+    console.log('Get function getemployees');
+    var database = req.db;
+    var employeeDB = database.get('employees');
+    var bid = req.user[0].business;
+    var businesses = db.get('businesses');
+    employeeDB.find( { business: bid })
+        .forEach(function(oneEmployee) {
+            console.log( "user: " + oneEmployee.fname );
+        });
 
     res.render('business/dashboard', {
         title: 'Express',
--- routes/webapp/business/getemployees.js
+++ routes/webapp/business/getemployees.js
@@ -1,52 +1,15 @@
 /**
  * Created by sean on 3/7/2016.
  */
-exports.get = function(req,res){
-    console.log('Get function addemployees');
-    var database =  req.db;
+exports.get = function(req,res) {
+    console.log('Get function getemployees');
+    var database = req.db;
     var employeeDB = database.get('employees');
-    var employee;
-    var notemployee;
-    var businessID = req.user[0].business.toString();
+    var bid = req.user[0].business;
+    var businesses = db.get('businesses');
+    employeeDB.find( { business: bid })
+        .forEach(function(oneEmployee) {
+            console.log( "user: " + oneEmployee.fname );
+        });
 
-    async.parallel({
-            employee: function(cb){
-                employeeDB.find({registrationToken: {$exists: false}, business: ObjectId(businessID)},function (err,results){
-
-                    if (err) { return next(err);  }
-                    if(!results) { return next(new Error('Error finding employee'));}
-
-                    employeee = results;
-                    console.log(employeee);
-                    cb();
-
-                });
-            },
-            nonemployee: function(cb){
-                employeeDB.find({registrationToken: {$exists: true}, business: ObjectId(businessID)}, function (err,results){
-
-
-                    if (err) { return next(err); }
-                    if(!results) { return next(new Error('Error finding employee'));}
-
-                    notemployee = results;
-                    cb();
-                });
-            }
-        },
-
-        function(err,results){
-
-            if(err){
-                throw err;
-            }
-            res.render('business/dashboard',
-                {
-                    title: 'Express',
-                    notsigned: notemployee,
-                    signed: employeee
-                }
-
-
-            )}
-    )};
+};
