7e77a66177e646a7a30a755378e4fb13128cbd76
Kurt Lang <kurtalang@gmail.com>
2016-03-08 15:57:13 -0800

    pull businesses from db for peter

--- routes/webapp/admin/admin.js
+++ routes/webapp/admin/admin.js
@@ -1,6 +1,16 @@
-exports.get = function(req, res) {
-    console.log('Get function admin');
-    res.render('admin/admin', {
-        title: 'sassyadmin'
-    });
-}
+exports.get = function (req, res) {
+    console.log('Get function get businesses');
+    var database = req.db;
+    var businessDB = database.get('businesses');
+    var bid = req.user[0].business;
+
+    businessDB.find()
+        .on('success', function(businesses) {
+
+            res.render('admin/admin', {
+                bizz: businesses,
+                message: req.flash("permission")
+            });
+
+        });
+};
