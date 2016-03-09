d61217259dbcffc31d4302c49325ed1d1d02de80
Kurt Lang <kurtalang@gmail.com>
2016-03-08 18:49:28 -0800

    Working regproc page

--- routes/webapp/admin/registerprocess.js
+++ routes/webapp/admin/registerprocess.js
@@ -1,26 +1,45 @@
 /**
  * Created by sean on 2/18/2016.
  */
+
 exports.get = function (req, res) {
     console.log('Inside register process get method');
-    if (!req.session.companyName) {
-        res.render('admin/registerprocess');
-    } else {
-        res.render('admin/registerprocess', {
-            //title: 'Express',
-            //companyName: req.session.companyName
+    var businessDB = req.db.get('businesses');
+    var bid = req.user[0].business;
+
+    businessDB.findById(bid)
+        .on('success', function(newBiz) {
+            res.render('admin/registerprocess', {
+                companyName: newBiz.companyName
+            });
         });
-    }
 };
+
 exports.post = function (req, res) {
-    var fname = req.body.fname;
+
     var companyName = req.body.companyName;
-    var email = req.body.email;
-    var password = req.body.password;
-    var username = req.body.username;
+    var companyAddress = req.body.companyAddress;
+    var phone = req.body.phone;
+
+    var businessDB = req.db.get('businesses');
+    var bid = req.user[0].business;
 
     console.log('Inside register process post method');
+    console.log(businessDB);
 
-    res.end(fname + companyName + email + password + username);
+    businessDB.findAndModify({
+        query: {_id: bid},
+        update: {companyName: companyName,
+            companyAddress: companyAddress,
+            phone: phone}
+    });
+
+    res.render('admin/registerprocess', {
+        companyName: companyName,
+        companyAddress: companyAddress,
+        phone: phone
+    });
+
+    //res.end(fname + companyName + email + password + username);
 
 };
